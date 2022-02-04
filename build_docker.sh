d244f10c3d886bb112908c7718a7dc0c83744763
Gerald Manipon <pymonger@gmail.com>
2022-02-03 17:52:32 -0800

    update (#1)

--- build_docker.sh
+++ build_docker.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-if [ "$#" -ne 4 ]; then
-  echo "Usage: $0 <tag> <github org> <github repo branch> <hysds release>"
-  echo "e.g.: $0 20170620 hysds master v4.0.1-beta.7"
-  echo "e.g.: $0 latest pymonger develop develop"
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <tag>"
+  echo "e.g.: $0 20170620"
+  echo "e.g.: $0 latest"
   exit 1
 fi
 TAG=$1
-ORG=$2
-BRANCH=$3
-HYSDS_RELEASE=$4
 
 
 # enable docker buildkit to allow build secrets
